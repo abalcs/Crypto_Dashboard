7b56c1fefd662e9696419dba558652276c85f34b
Alan Balcom <abalcom23@gmail.com>
2022-12-04 23:48:34 -0500

    first commit

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function Nav() {
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar 
+      position="static" 
+      style={{ background: '#212121' }}>
+        <Toolbar>
+          <Typography 
+          variant="h5" 
+          component="div" 
+          sx={{ flexGrow: 1 }} 
+          style={{ color: '#ffc400', fontWeight: '800', paddingLeft: '5%' }}>
+            Crypto Dash
+          </Typography>
+          <Button 
+          variant='contained' 
+          style={{ background: '#ffc400', color: '#000', marginRight: '5%' }}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
